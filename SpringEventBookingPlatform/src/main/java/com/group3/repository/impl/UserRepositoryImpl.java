9f11acd7c4e8ca53aaa8dd2241a6acc959655e92
Nguyễn Thuận <thuantokutou@gmail.com>
2026-05-01 00:15:41 +0700

    feat: add query user + role and add auth api

---
+++ SpringEventBookingPlatform/src/main/java/com/group3/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,57 @@
+package com.group3.repository.impl;
+
+import com.group3.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import com.group3.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import jakarta.data.repository.Repository;
+import jakarta.transaction.Transactional;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author THUAN
+ */
+@Repository
+@Transactional
+public class UserRepositoryImpl implements UserRepository{
+    @Autowired
+    private LocalSessionFactoryBean factory;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public User getUserByUsername(String username) {
+        Session session = this.factory.getObject().getCurrentSession();
+        Query q = session.createNamedQuery("User.findByUsername", User.class);
+        q.setParameter("username", username);
+
+        return (User) q.getSingleResult();
+
+    }
+
+    @Override
+    public User addUser(User u) {
+        Session session = this.factory.getObject().getCurrentSession();
+        session.persist(u);
+        
+        return u;
+    }
+
+    @Override
+    public boolean authenticate(String username, String password) {
+        User u = this.getUserByUsername(username);
+
+        return this.passwordEncoder.matches(password, u.getPassword());
+    }
+}
