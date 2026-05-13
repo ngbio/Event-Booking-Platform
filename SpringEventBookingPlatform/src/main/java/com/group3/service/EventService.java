c3cf9301301284b11af28c909b42fca45e9b984e
maiithanhai <maiithanhai@gmail.com>
2026-05-13 15:30:58 +0700

    refactor: update API_docs.md and add dependency Security-web

--- SpringEventBookingPlatform/src/main/java/com/group3/service/EventService.java
+++ SpringEventBookingPlatform/src/main/java/com/group3/service/EventService.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.group3.service;
 
 import com.group3.pojo.Event;
@@ -10,10 +6,6 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.web.multipart.MultipartFile;
 
-/**
- *
- * @author THUAN
- */
 public interface EventService {
     List<Event> getEvents(Map<String, String> params);
     Event getEventById(Integer id);
