aa47953c248ef90bb5baa05df45b025ba3c9c6c2
Nguyễn Thuận <thuantokutou@gmail.com>
2026-05-24 16:32:07 +0700

    fix: error swagger-UI

--- SpringEventBookingPlatform/src/main/java/com/group3/configs/OpenApiConfig.java
+++ SpringEventBookingPlatform/src/main/java/com/group3/configs/OpenApiConfig.java
@@ -4,11 +4,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.configuration.SpringDocConfiguration;
-import org.springdoc.core.configuration.SpringDocUIConfiguration;
 import org.springdoc.core.properties.SpringDocConfigProperties;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springdoc.core.properties.SwaggerUiOAuthProperties;
+import org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration;
 import org.springdoc.webmvc.ui.SwaggerConfig;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -19,8 +21,8 @@ import org.springframework.context.annotation.Import;
     SpringDocConfigProperties.class,
     SwaggerUiConfigProperties.class,
     SwaggerUiOAuthProperties.class,
+    SpringDocWebMvcConfiguration.class,
     SwaggerConfig.class,
-    SpringDocUIConfiguration.class
 })
 public class OpenApiConfig {
 
@@ -33,5 +35,15 @@ public class OpenApiConfig {
                         .description("REST API documentation for Spring Event Booking Platform.")
                         .contact(new Contact()
                                 .name("Group 3")));
+    }
+
+    @Bean
+    public WebProperties webProperties() {
+        return new WebProperties();
+    }
+
+    @Bean
+    public WebMvcProperties webMvcProperties() {
+        return new WebMvcProperties();
     }
 }
