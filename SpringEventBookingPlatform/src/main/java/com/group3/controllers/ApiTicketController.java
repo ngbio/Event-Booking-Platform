1544dd138217471a1fe9d0dd4029d85e39d83255
maiithanhai <maiithanhai@gmail.com>
2026-05-26 16:02:48 +0700

    refactor: create controllers

---
+++ SpringEventBookingPlatform/src/main/java/com/group3/controllers/ApiTicketController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.group3.controllers;
+
+/**
+ *
+ * @author thanh
+ */
+public class ApiTicketController {
+//    @RequestMapping("/api/secure/tickets")
+//Chứa các API (Dòng 32 - 35):
+/// (Danh sách vé đã mua)
+/// qr/{qrCode} (Tra cứu)
+/// checkin (Organizer check-in)
+}
